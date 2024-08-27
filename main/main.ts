e0e26422708beb3ed8a84ecc389256de4c60efac
Roman Havrylko <groman2000@gmail.com>
2024-08-28 00:59:32 +0300

    feat: improved save-highlight button

--- main/main.ts
+++ main/main.ts
@@ -24,29 +24,39 @@ const externalSiteViewCreate = () => {
   externalView.webContents.on('did-finish-load', () => {
     externalView.webContents.executeJavaScript(`
       document.addEventListener('mouseup', () => {
+        setTimeout(() => {
         const selection = window.getSelection();
-        if (selection && selection.toString().length > 0) {
-          const selectedText = selection.toString();
-          const popup = document.createElement('div');
-          popup.setAttribute('id', 'button_popup');
-          popup.style.position = 'absolute';
-          popup.style.backgroundColor = '#FFF';
-          popup.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
-          popup.style.border = '1px solid #000';
-          popup.style.borderRadius = '0.375rem';
-          popup.style.padding = '5px';
-          popup.style.zIndex = '10000';
-          popup.style.top = \`\${window.pageYOffset + selection.getRangeAt(0).getBoundingClientRect().top + 10}px\`;
-          popup.style.left = \`\${selection.getRangeAt(0).getBoundingClientRect().left + popup.getBoundingClientRect().width}px\`;
-          popup.textContent = 'Save Highlight';
-          popup.onclick = (el) => {
-            const title = document.title;
-            const url = window.location.href;
-            window.electron.send('save-highlight', { url, title, text: selectedText });
-            el.target.remove();
-          };
-          document.body.appendChild(popup);
+        if (!selection || selection.toString().length === 0) return;
+        const selectedText = selection.toString();
+        const popup = document.createElement('button');
+        popup.setAttribute('id', 'button_popup');
+        popup.style.position = 'absolute';
+        popup.style.backgroundColor = '#FFF';
+        popup.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+        popup.style.border = '1px solid #000';
+        popup.style.borderRadius = '0.375rem';
+        popup.style.padding = '5px';
+        popup.style.zIndex = '10000';
+        popup.style.top = \`\${window.pageYOffset + selection.getRangeAt(0).getBoundingClientRect().top + 10}px\`;
+        popup.style.left = \`\${selection.getRangeAt(0).getBoundingClientRect().left + popup.getBoundingClientRect().width}px\`;
+        popup.textContent = 'Save Highlight';
+        popup.onclick = function() {
+          const title = document.title;
+          const url = window.location.href;
+          window.electron.send('save-highlight', { url, title, text: selectedText });
+          selection.removeAllRanges();
+          this.remove();
+        };
+        popup.addEventListener("focus", function () {
+          this.style.borderColor = "orange";  
+        });
+        popup.onblur = function () {
+            console.log('onblur');
+            this.remove();
         }
+        document.body.appendChild(popup);
+        popup.focus();
+        }, 0);
       });
     `);
   });
@@ -107,8 +117,6 @@ const createWindow = async () => {
 
   externalSiteViewCreate();
 };
-
-
 
 app.whenReady().then(() => {
   createWindow();
